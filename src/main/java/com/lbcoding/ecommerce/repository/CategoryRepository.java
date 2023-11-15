064725444192fc7d58e31e2bebff0b343d707d21
Leonid10011 <leonid.budkov@gmail.com>
2023-11-15 11:41:28 +0100

    Cleaned CategoryRepository

--- src/main/java/com/lbcoding/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/lbcoding/ecommerce/repository/CategoryRepository.java
@@ -2,12 +2,12 @@ package com.lbcoding.ecommerce.repository;
 
 import com.lbcoding.ecommerce.model.Category;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
+import jakarta.persistence.*;
 import jakarta.transaction.Transactional;
+import jakarta.ws.rs.NotFoundException;
 import org.jboss.resteasy.reactive.server.multipart.MultipartFormDataInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,65 +15,40 @@ import java.util.Optional;
 
 @ApplicationScoped
 public class CategoryRepository {
-
+    public static final Logger logger = LoggerFactory.getLogger(CategoryRepository.class);
     @PersistenceContext
     private EntityManager entityManager;
-
-    /**
-     * Find category by ID
-     * @param id
-     * @return
-     */
-    public Optional<Category> findById(Long id){
-
-        Optional<Category> category;
-
-        try {
-            Category result = entityManager.find(Category.class, id);
-            category = Optional.of(result);
-        } catch(Exception e){
-            category = Optional.empty();
+    @Transactional
+    public void create(Category category){
+        if(category == null){
+            throw new IllegalArgumentException("Category cannot be null");
         }
-
-        return category.isEmpty() ? Optional.empty() : category;
+        logger.info("Persisting new category");
+        entityManager.persist(category);
+        logger.info("Category persisted successfully with ID: " + category.getId());
+    }
+    public Optional<Category> findById(Long id){
+        return Optional.ofNullable(entityManager.find(Category.class, id));
     }
 
     /**
-     * Get all categories
-     * @return
+     * Get all categories if there are any. Otherwise throw an NotFoundException.
+     * @return List<Category> - A list containing all categories. If empty, throw an NotFoundException
      */
     @Transactional
     public List<Category> get(){
-        String jpql = "SElECT c FROM Category c";
-
-        TypedQuery<Category> query = entityManager.createQuery(jpql, Category.class);
-
+        logger.info("Querying to get all categories");
+        TypedQuery<Category> query = entityManager.createQuery(
+                "SELECT c FROM Category c", Category.class
+        );
         List<Category> categories = query.getResultList();
-
-        return categories.isEmpty() ? null : categories;
-
+        if(categories.isEmpty()){
+            logger.info("No categories found");
+            return categories;
+        }
+        logger.info("Categories found");
+        return categories;
     }
-
-    /**
-     * Create new category
-     * @param category
-     */
-    @Transactional
-    public void create(Category category){
-        entityManager.persist(category);
-    }
-
-    /**
-     * Delete category by id
-     * @param id
-     */
-    @Transactional
-    public void delete(Long id){
-        Optional<Category> category = findById(id);
-
-        category.ifPresent(entityManager::remove);
-    }
-
     /**
      * Get category by name
      * @param name
@@ -81,19 +56,29 @@ public class CategoryRepository {
      */
     @Transactional
     public Optional<Category> findByName(String name) {
+        logger.info("Querying find category by name " + name);
         TypedQuery<Category> query = entityManager.createQuery(
                 "SELECT c FROM Category c WHERE c.name = :name", Category.class);
         query.setParameter("name", name);
 
-        Optional<Category> category;
-
         try {
             Category result = query.getSingleResult();
-            category = Optional.of(result);
-        } catch( Exception e){
-            category = Optional.empty();
+            logger.info("Category found by name " + name);
+            return Optional.of(result);
+        } catch( NoResultException e){
+            logger.info("Category not found for name " + name);
+            throw new NotFoundException("Category not found");
         }
-
-        return category.isEmpty() ? Optional.empty() : category;
+    }
+    /**
+     * Delete category by id
+     * @param id
+     */
+    @Transactional
+    public void delete(Long id){
+        logger.info("Deleting category by ID: " + id);
+        Category category = findById(id).orElseThrow(() -> new NotFoundException("Category with ID " + id + " not found"));
+        entityManager.remove(category);
+        logger.info("Category deleted successfully");
     }
 }
